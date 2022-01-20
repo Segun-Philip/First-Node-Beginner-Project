a53b27292545c25154992734a3c44d23ab24ee6f
Segun-Philip <philipojediran@gmail.com>
2022-01-20 10:50:41 +0100

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+//GLOBALS       -   NO WINDOW
+
+//__dirname     -   path to current directory
+//__filename    -   file name
+// require      -   function to use modules(ComonJS)
+// module       -   info about current module(file)
+// process      -   info about env where  the program is being excuted
+
+//console.log(GLOBALS)
+setInterval(() => {
+    console.log("Hello world")
+}, 1000)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+res.end(`
+    // <h1>Oops!</h1>
+    // <p>We can't seem to find the page you are looking for</p>
+    // <a href="/">back home</a>
+    // `)
